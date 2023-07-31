3b5dfae6aa141f98891e9245cf840dfd078ba9d3
Aniello Paolo Malinconico <63e4d113fb75f8568f6487ed@bots.bitbucket.org>
2023-07-31 22:48:20 +0200

    added redis + 2 rest api

--- main.go
+++ main.go
@@ -1,12 +1,104 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 
-func main() {
-	msg := sayHello("Alice")
-	fmt.Println(msg)
+	"github.com/go-redis/redis"
+	"github.com/gorilla/mux"
+)
+
+// Document represents the structure of a document.
+type Document struct {
+	ID   string `json:"id"`
+	Text string `json:"text"`
+}
+
+var redisClient *redis.Client
+
+func initRedis() {
+	redisClient = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379", // Replace this with your Redis server address
+		DB:   0,
+	})
+
+	_, err := redisClient.Ping().Result()
+	if err != nil {
+		log.Fatalf("Failed to connect to Redis: %v", err)
+	}
+}
+
+// StoreDocumentHandler handles storing a new document in Redis.
+func StoreDocumentHandler(w http.ResponseWriter, r *http.Request) {
+	var document Document
+
+	err := json.NewDecoder(r.Body).Decode(&document)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	if document.ID == "" {
+		http.Error(w, "Document ID must not be empty", http.StatusBadRequest)
+		return
+	}
+
+	jsonData, err := json.Marshal(document)
+	if err != nil {
+		http.Error(w, "Error converting document to JSON", http.StatusInternalServerError)
+		return
+	}
+
+	err = redisClient.Set(document.ID, jsonData, 0).Err()
+	if err != nil {
+		http.Error(w, "Failed to store document in Redis", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+// GetDocumentHandler retrieves a document from Redis based on the provided ID.
+func GetDocumentHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	docID := params["id"]
+
+	val, err := redisClient.Get(docID).Result()
+	if err == redis.Nil {
+		http.Error(w, "Document not found", http.StatusNotFound)
+		return
+	} else if err != nil {
+		http.Error(w, "Failed to retrieve document from Redis", http.StatusInternalServerError)
+		return
+	}
+
+	var document Document
+	err = json.Unmarshal([]byte(val), &document)
+	if err != nil {
+		http.Error(w, "Error unmarshalling document from JSON", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(document)
 }
 
 func sayHello(name string) string {
 	return fmt.Sprintf("Hello %s", name)
+}
+
+func main() {
+	msg := sayHello("Alicea")
+	fmt.Println(msg)
+
+	initRedis()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/documents", StoreDocumentHandler).Methods("POST")
+	router.HandleFunc("/documents/{id}", GetDocumentHandler).Methods("GET")
+
+	port := 8080
+	fmt.Printf("Server listening on port %d...\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
 }
